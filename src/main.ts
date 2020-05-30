92c601afc7915bdbb1b63d139b5a4355c7699fbe
sj48637 <jingjing994@qq.com>
2020-05-30 11:19:38 +0800

    接入antd

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueCompositionApi from '@vue/composition-api'
+
+import { Button } from 'ant-design-vue'
+
+Vue.component(Button.name, Button)
+Vue.use(VueCompositionApi)
 
 Vue.config.productionTip = false
 

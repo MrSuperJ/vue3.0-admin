ba0262eee63426376e66a81c08eb8dda817c13cd
sj48637 <jingjing994@qq.com>
2020-05-31 11:11:18 +0800

    公共调整

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+import './assets/styles/index.less'
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import VueCompositionApi from '@vue/composition-api'
-
 import { Button } from 'ant-design-vue'
 
 Vue.component(Button.name, Button)
